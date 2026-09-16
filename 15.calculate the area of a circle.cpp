8f8cfbf85c9be3dfe13e3608a4a76945b1a46421
Yesmin Akter Lamia <yesmin.akter.lamia@gmail.com>
2026-09-17 02:15:03 +0600

    15.calculate the area of a circle.cpp

---
+++ 15.calculate the area of a circle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float r,area;
+    cout << "Radius: ";
+    cin>>r;
+    area=3.1416*r*r;
+    cout << "Area of circle = " << area;
+    return 0;
+}
