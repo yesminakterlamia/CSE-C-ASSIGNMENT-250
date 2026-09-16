b63226d9cc30c805643dcca022e2d1040c6bd72b
Yesmin Akter Lamia <yesmin.akter.lamia@gmail.com>
2026-09-17 02:38:39 +0600

    17.calculate the area of a rectangle.cpp

---
+++ 17.calculate the area of a rectangle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float l,w,area;
+    cout<<"L and W:";
+    cin>>l>>w;
+    area=l*w;
+    cout << "Area of rectangle = " << area;
+    return 0;
+}
