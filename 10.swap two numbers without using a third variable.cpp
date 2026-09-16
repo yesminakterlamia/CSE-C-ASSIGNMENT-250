2f83604645724e05c9aee44730f3faf94776b076
Yesmin Akter Lamia <yesmin.akter.lamia@gmail.com>
2026-09-17 01:13:01 +0600

    10.swap two numbers without using a third variable.

---
+++ 10.swap two numbers without using a third variable.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin >>a>>b;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout <<"After swapping: a="<<a<<",b="<<b;
+    return 0;
+}
+
