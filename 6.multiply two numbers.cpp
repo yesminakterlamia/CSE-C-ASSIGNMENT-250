67c43ec31f0975e7492fab8f35faf1eff39ad184
Yesmin Akter Lamia <yesmin.akter.lamia@gmail.com>
2026-09-17 00:06:35 +0600

    6.multiply two numbers.cpp

---
+++ 6.multiply two numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, multiply;
+    cin >> a >> b;
+    multiply = a * b;
+    cout << "Multiply = " << multiply;
+    return 0;
+}
+
