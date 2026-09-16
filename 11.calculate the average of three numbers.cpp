701a4f6b244ecde666df2c13214f9df09573bd15
Yesmin Akter Lamia <yesmin.akter.lamia@gmail.com>
2026-09-17 01:21:27 +0600

    11.calculate the average of three numbers.cpp

---
+++ 11.calculate the average of three numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    float average;
+    cin >>a>>b>>c;
+    average=(a+b+c)/3.0;
+    cout<<"Average = "<<average;
+    return 0;
+}
