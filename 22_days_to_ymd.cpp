0216d95273968deb130d5862bb48308f685f3c66
Yesmin Akter Lamia <yesmin.akter.lamia@gmail.com>
2026-09-19 19:43:16 +0600

    Create 22_days_to_ymd.cpp

---
+++ 22_days_to_ymd.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int totalDays;
+ cout << "Enter total number of days: ";
+    cin >> totalDays;
+
+int years = totalDays / 365;
+    int remainingAfterYears = totalDays % 365;
+
+int months = remainingAfterYears / 30;
+    int days = remainingAfterYears % 30;
+
+ cout << totalDays << " days = "
+         << years << " years, "
+   << months << " months, "
+       << days << " days" << endl;
+    return 0;
+}
