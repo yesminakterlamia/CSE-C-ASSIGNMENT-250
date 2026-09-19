877eb223ecb703fbfb0419085c178cc87f8305f4
Yesmin Akter Lamia <yesmin.akter.lamia@gmail.com>
2026-09-19 19:39:01 +0600

    Create 18_perimeter_of_rectangle.cpp

---
+++ 18_perimeter_of_rectangle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double length, width;
+
+  cout << "Enter length of rectangle: ";
+    cin >> length;
+ cout << "Enter width of rectangle: ";
+ cin >> width;
+
+    double perimeter = 2 * (length + width);
+  cout << "Perimeter of rectangle = " << perimeter << endl;
+    return 0;
+}
