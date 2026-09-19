4dc518c86a812690ffd03a4bce5adf325cb4daea
Yesmin Akter Lamia <yesmin.akter.lamia@gmail.com>
2026-09-19 19:43:02 +0600

    Create 19_simple_interest.cpp

---
+++ 19_simple_interest.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double principal, rate, time;
+
+ cout << "Enter principal amount: ";
+  cin >> principal;
+    cout << "Enter annual interest rate (%): ";
+  cin >> rate;
+ cout << "Enter time (in years): ";
+    cin >> time;
+
+ double simpleInterest = (principal * rate * time) / 100;
+    cout << "Simple Interest = " << simpleInterest << endl;
+    return 0;
+}
